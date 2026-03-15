3a05ff03a5752b9c55007ce61afdb2d4f0c979da
Alan Night <a.nightails@gmail.com>
2026-03-15 14:22:52 -0500

    Have GetDeviceInfo return a Device object instead of placeholder String

--- internal/elgato/device.go
+++ internal/elgato/device.go
@@ -16,20 +16,20 @@ type Device struct {
 	DisplayName         string `json:"displayName,omitempty"`
 }
 
-func GetDeviceInfo(url string) (string, error) {
+func GetDeviceInfo(url string) (Device, error) {
 	resp, err := http.Get(url)
 	if err != nil {
-		return "", err
+		return Device{}, err
 	}
 	defer resp.Body.Close()
 	if resp.StatusCode != http.StatusOK {
-		return "", fmt.Errorf("status code error: %d %s", resp.StatusCode, resp.Status)
+		return Device{}, fmt.Errorf("status code error: %d %s", resp.StatusCode, resp.Status)
 	}
 
 	var device Device
 	if err := json.NewDecoder(resp.Body).Decode(&device); err != nil {
-		return "", fmt.Errorf("failed to decode response body: %v", err)
+		return Device{}, fmt.Errorf("failed to decode response body: %v", err)
 	}
 
-	return device.SerialNumber, nil
+	return device, nil
 }
